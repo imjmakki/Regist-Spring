fa4d26632164c818cd0444fd44f6492a56b7f09a
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 16:36:57 +0300

    Regist

---
+++ src/test/java/app/regist/regist/RegistApplicationTests.java
@@ -0,0 +1,13 @@
+package app.regist.regist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
